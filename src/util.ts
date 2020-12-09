841f4e2ade632f6c44184afb5503839902bb4f35
xg4 <xingor4@gmail.com>
2020-12-09 07:23:48 +0000

    fix: don't write files, if these is no diff

--- src/util.ts
+++ src/util.ts
@@ -27,19 +27,25 @@ export async function exists(path: string) {
   }
 }
 
-export async function diffFile(fullPath: string, trList: string[][]) {
+async function getFileContent(fullPath: string) {
   let source = ''
   if (await exists(fullPath)) {
     const fileText = await readFile(fullPath)
     const decoder = new TextDecoder()
     source = decoder.decode(fileText)
   }
+  return source
+}
 
+export async function diffFile(fullPath: string, trList: string[][]) {
+  const source = await getFileContent(fullPath)
   const diffList = diffContent(source, trList)
 
-  const diffStr = diffList.map((td) => td.join(' | ')).join('\n')
-  const newSource = diffStr + '\n' + source
-  await writeFile(fullPath, newSource)
+  if (diffList.length) {
+    const diffStr = diffList.map((td) => td.join(' | ')).join('\n')
+    const newSource = source ? diffStr + '\n' + source : diffStr
+    await writeFile(fullPath, newSource)
+  }
 
   return diffList
 }
