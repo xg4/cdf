2fb0e98847c887aa10d2577bf2f3230efcfce1f6
xg4 <xingor4@gmail.com>
2020-12-10 04:55:48 +0000

    ci: update

--- git-status.sh
+++ git-status.sh
@@ -5,4 +5,9 @@ if [ -n "$(git diff --exit-code)" ]; then
   exit 0
 else
   echo "there are changes";
+  git config user.name github-actions[bot]
+  git config user.email github-actions[bot]@users.noreply.github.com
+  git add .
+  git commit -m "chore: update data source"
+  git push
 fi
