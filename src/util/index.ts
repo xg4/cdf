ea15a465944ed8359e059b294e4f6ef53aba4410
xg4 <xingor4@gmail.com>
2020-12-14 22:05:22 +0800

    chore: update renderContent

--- src/util/index.ts
+++ src/util/index.ts
@@ -7,26 +7,26 @@ export function renderContent([
   uuid,
   _,
   region,
-  project,
-  __,
-  range,
-  count,
-  phone,
-  startDate,
-  endDate,
+  name,
+  licenseNumber,
+  details,
+  number,
+  phoneNumber,
+  startsAt,
+  endsAt,
   ___,
   status,
 ]: string[]) {
   return [
-    `${region} ${project} ${status}`,
+    `${region} ${name} ${status}`,
     `
 ### 区域  \n${region}  \n
-### 项目名称  \n${project}  \n
-### 预售范围  \n${range}  \n
-### 住房套数  \n${count}  \n
-### 开发商咨询电话  \n${phone}  \n
-### 登记开始时间：  \n${startDate}  \n
-### 登记结束时间：  \n${endDate}  \n
+### 项目名称  \n${name}  \n
+### 预售范围  \n${details}  \n
+### 住房套数  \n${number}  \n
+### 开发商咨询电话  \n${phoneNumber}  \n
+### 登记开始时间：  \n${startsAt}  \n
+### 登记结束时间：  \n${endsAt}  \n
 ### 状态：  \n${status}
 `.trim(),
   ]
